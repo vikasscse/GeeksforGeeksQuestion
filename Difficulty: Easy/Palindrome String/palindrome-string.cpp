317cc5471a60f92b52fb7d3cd4b8ba87609c62ad
vikasscse <vishalkumarstm497@gmail.com>
2026-08-14 18:36:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.cpp
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -1,33 +1,18 @@
 class Solution {
   public:
+  
+    bool fun(int i,int j,string&s)
+    {
+        if(i>=j)
+        return true;
+        
+        if(s[i++]!=s[j--])
+        return false;
+        
+        fun(i,j,s);
+    }
     bool isPalindrome(string& s) {
         // code here
-    //     int n=s.size();
-    //     char y[n];
-    //     for(int i=0;i<n;i++){
-    //         y[i]=s[i];
-    //     }
-    //     int start=0;
-    //     int end=n-1;
-    //   while(start<end){
-    //       int temp=s[start];
-    //       s[start]=s[end];
-    //       s[end]=temp;
-    //       start++;
-    //       end--;
-    //   }
-    //   for(int i=0;i<n;i++){
-    //       if(y[i]!=s[i])
-    //       return false;
-    //   }
-    //   return true;
-    int i=0;int j=s.size()-1;
-    while(i<j)
-    {
-        if(s[i]!=s[j]) return false;
-        else i++;
-        j--;
-    }
-    return true;
+        fun(0,s.size()-1,s);
     }
 };
